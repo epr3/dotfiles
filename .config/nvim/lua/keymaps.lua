f5435f6853c065e2fef29ba89d4376e0741480bf
Eduard Predescu <eduard.florin.predescu@gmail.com>
2024-05-22 19:11:28 +0300

    feature: add cowboy util

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -48,4 +48,40 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+local function cowboy()
+  local id
+  local ok = true
+  for _, key in ipairs { 'h', 'j', 'k', 'l', '+', '-' } do
+    local count = 0
+    local timer = assert(vim.uv.new_timer())
+    local map = key
+    vim.keymap.set('n', key, function()
+      if vim.v.count > 0 then
+        count = 0
+      end
+      if count >= 10 and vim.bo.buftype ~= 'nofile' then
+        ok, id = pcall(vim.notify, 'Hold it Cowboy!', vim.log.levels.WARN, {
+          icon = '🤠',
+          replace = id,
+          keep = function()
+            return count >= 10
+          end,
+        })
+        if not ok then
+          id = nil
+          return map
+        end
+      else
+        count = count + 1
+        timer:start(2000, 0, function()
+          count = 0
+        end)
+        return map
+      end
+    end, { expr = true, silent = true })
+  end
+end
+
+cowboy()
+
 -- vim: ts=2 sts=2 sw=2 et
