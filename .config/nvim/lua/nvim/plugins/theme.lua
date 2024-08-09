d1710a885c8ca628b0b14ead1103a7dcc1567976
Eduard Predescu <eduard.florin.predescu@gmail.com>
2024-08-09 20:44:22 +0300

    chore: update theme to catpuccin

--- .config/nvim/lua/nvim/plugins/theme.lua
+++ .config/nvim/lua/nvim/plugins/theme.lua
@@ -6,16 +6,14 @@ return {
       --
       -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
 
-      'navarasu/onedark.nvim',
+      'catppuccin/nvim',
+      name = 'catppuccin',
       priority = 1000, -- Make sure to load this before all the other start plugins.
-      opts = {
-        style = 'deep',
-      },
       init = function()
         -- Load the colorscheme here.
         -- Like many other themes, this one has different styles, and you could load
         -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-        vim.cmd.colorscheme 'onedark'
+        vim.cmd.colorscheme 'catppuccin-macchiato'
       end,
     },
   },
